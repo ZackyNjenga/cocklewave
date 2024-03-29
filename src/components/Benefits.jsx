cbc13823ce26f4ab9fec145ca118ce9fcba61743
zacharynjenga381@gmail.com <zacharynjenga381@gmail.com>
2024-03-30 01:09:48 +0300

    Just a logo

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -6,30 +6,19 @@ import {benefits} from "../constants/index.jsx";
 const Benefits = () => {
     return (
         <Section id="features">
-            <div className="container relative z-2">
+            <div className="container relative">
                 <Heading
-                    className=" md:max-w-md lg:max-w-2xl"
-                    title="Chat smarter Not Harder with cocklewave"
-                />
+                    className="md:max-w-md lg:max-w-2xl"
+                    title="Chat smarter not Harder with cocklewave"/>
+
                 <div className="flex flex-wrap gap-10 mb-10">
-                    {benefits.map((item) =>(
-                        <div
-                            className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%]
-                            md:max-w-[24rem]"
-                            style={{
-                                backgroundImage: `url(${item},
-                                backgroundUrl
-                                    
-                            }
-                            key={item.id}>
+                    {benefits.map((item) => (
+                        <div key={item.id}>
                             <div>
-                                <h5>
-                                    {item.title}
-                                </h5>
+                                <h5>{item.title}</h5>
                             </div>
                         </div>
-                        )
-                    )}
+                    ))}
                 </div>
             </div>
         </Section>
